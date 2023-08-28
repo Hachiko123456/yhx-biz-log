faf57c9a77cfdf8e339f79767f478271d1afe442
yanghuaxu <1769969562@qq.com>
2023-08-28 17:52:04 +0800

    [fix]增加包扫描

---
+++ yhx-csb-log/src/main/java/com/cvte/yhx/log/config/MapperScanConfig.java
@@ -0,0 +1,15 @@
+package com.cvte.yhx.log.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yanghuaxu
+ * @date 2023/8/28 17:48
+ */
+@Configuration
+@MapperScan("com.cvte.yhx.log.mapper")
+@ComponentScan(basePackages = {"com.cvte.yhx.log"})
+public class MapperScanConfig {
+}
